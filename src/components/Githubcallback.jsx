dddaa33df2455afcbf3ca1bd7db54ccf76dc9a84
ennter usernane <vruthvik.chinthoju@gmail.com>
2026-03-30 11:32:55 +0530

    fixed github call back

--- src/components/Githubcallback.jsx
+++ src/components/Githubcallback.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./css/githubcallback.css"
 
 function GithubCallback() {
   const navigate = useNavigate();
